8ff1371cd3812725aedc9d368457f460a30bd5d3
Heba Fayad <heba.fayad.dobai@gmail.com>
2019-12-15 12:05:42 +0000

    Automatically backed up by Learn

--- src/Bomb.js
+++ src/Bomb.js
@@ -3,7 +3,12 @@ import React, { Component } from 'react';
 export default class Bomb extends Component
 { 
   
-  
+   constructor(props){
+      super(props)
+      this.state = {
+        secondsLeft: this.props.initialCount 
+      }
+  }  
   
   
   render(){
