5021c4415de9fa72000603b98d6f49bf26a20da3
Heba Fayad <heba.fayad.dobai@gmail.com>
2019-12-15 12:06:46 +0000

    Automatically backed up by Learn

--- src/Bomb.js
+++ src/Bomb.js
@@ -6,7 +6,7 @@ export default class Bomb extends Component
    constructor(props){
       super(props)
       this.state = {
-        secondsLeft: this.props.initialCount 
+        secondsLeft: props.initialCount 
       }
   }  
   
